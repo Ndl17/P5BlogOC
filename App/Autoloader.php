429eba89af839dbde50b746753df3d83a13f4692
Ndl <juliengautiernadal@hotmail.com>
2023-01-19 15:26:29 +0100

    minor correction, autoloader PHPDOC

--- App/Autoloader.php
+++ App/Autoloader.php
@@ -1,8 +1,19 @@
 <?php
 namespace App;
 
+/**
+ * Classe Autoloader pour charger automatiquement les classes dans le projet.
+ */
+
+
 class Autoloader
 {
+
+
+  /**
+   * Enregistrer l'autoloader
+   * @return void
+   */
   public static function register()
   {
     spl_autoload_register([
@@ -11,6 +22,12 @@ class Autoloader
     ]);
   }
 
+
+  /**
+   * Charger automatiquement la classe
+   * @param string $class
+   * @return void
+   */
   public  static function autoload($class){
     $class = str_replace(__NAMESPACE__. '\\','',$class);
     $class = str_replace('\\','/',$class);
